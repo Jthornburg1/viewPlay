4d97f2b3e2b9297aebc0299d8c6b3475b467d300
Jon Thornburg <reliable2192@gmail.com>
2015-04-27 15:38:11 -0600

    Initial Commit

---
+++ ViewPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewPlay
+//
+//  Created by jonathan thornburg on 4/27/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
