adde18eb9905a7f615012f35d68dd44569fad7c1
Jon Thornburg <reliable2192@gmail.com>
2015-04-30 22:47:25 -0600

    Issues

--- ViewPlay/AppDelegate.m
+++ ViewPlay/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+                 
+                    self.window.rootViewController = [ViewController new];
+    
+            
+                   
+                   self.window.backgroundColor = [UIColor blueColor];
+                    [self.window makeKeyAndVisible];
+    
+    
+    
     return YES;
 }
 
--- ViewPlay/ViewController.m
+++ ViewPlay/ViewController.m
@@ -17,6 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    CGFloat squareSize;
+    squareSize = CGRectGetWidth(self.view.frame) / 2.0;
+    
+    UIView *view1 = [[UIView alloc] init];
+    view1.frame = CGRectMake(0, 0, squareSize, squareSize);
+    view1.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view1];
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.frame = CGRectMake(squareSize, 0, squareSize, squareSize);
+    view1.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:view2];
+    
+    UIView *view3 = [[UIView alloc] init];
+    view3.frame = CGRectMake(0, squareSize, squareSize, squareSize);
+    view1.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:view3];
+    
+    UIView *view4 = [[UIView alloc] init];
+    view4.frame = CGRectMake(squareSize, squareSize, squareSize, squareSize);
+    view1.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:view4];
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
